95dff17426f9247d72ff23866653d09a638f6263
Eugene Feshchenko <efeshchenko@milosolutions.com>
2016-11-04 16:26:57 +0200

    hmm....

--- app/weight-settings/weight-settings.component.js
+++ app/weight-settings/weight-settings.component.js
@@ -4,7 +4,22 @@ angular.
     templateUrl: 'weight-settings/weight-settings.template.html',
     controller: ['$routeParams',
       function settingsController($routeParams) {
-        
+        params = localStorage['params'];
+        if !(params){
+        	var params = {'weight':{'start': 0,
+                                    'target': 0},
+                          'dates':{'start':0,
+                                   'end': 0},
+                          }
+        }
+      }
+      this.start_weight = params['weight']['start'];
+      this.target_weight = params['weight']['target'];
+      this.start_date = new Date(params['dates']['start']);
+      this.end_date = new Date(params['dates']['end']);
+
+      this.save = function(){
+      	localStorage['params'] = params;
       }
     ]
   });
