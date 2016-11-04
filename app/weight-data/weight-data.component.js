ec35632cab63f1cb074d7caa3af1b219c8cf9a30
Eugene Feshchenko <efeshchenko@milosolutions.com>
2016-11-04 12:39:19 +0200

    initial

---
+++ app/weight-data/weight-data.component.js
@@ -0,0 +1,21 @@
+angular.
+  module('weightData').
+  component('weightData', {
+    templateUrl:'weight-data/weight-data.template.html',
+    controller: function WeightDataController() {
+      this.records = [
+           {'date': new Date(04/11/2016),
+            'weight': 99.5,
+            'note': '',
+            },
+          {'date': new Date(03/11/2016),
+           'weight': 99.0,
+           'note': '',
+           },
+          {'date': new Date(02/11/2016),
+           'weight': 98.8,
+           'note': '',
+          },
+         ]
+       }
+  });
