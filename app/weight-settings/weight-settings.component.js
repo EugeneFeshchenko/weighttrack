e013c1cc2c733c9c26089170789000f695173d5f
Eugene Feshchenko <efeshchenko@milosolutions.com>
2016-11-05 10:33:28 +0200

    settings page

--- app/weight-settings/weight-settings.component.js
+++ app/weight-settings/weight-settings.component.js
@@ -4,22 +4,38 @@ angular.
     templateUrl: 'weight-settings/weight-settings.template.html',
     controller: ['$routeParams',
       function settingsController($routeParams) {
-        params = localStorage['params'];
-        if !(params){
-        	var params = {'weight':{'start': 0,
-                                    'target': 0},
-                          'dates':{'start':0,
-                                   'end': 0},
-                          }
-        }
-      }
-      this.start_weight = params['weight']['start'];
-      this.target_weight = params['weight']['target'];
-      this.start_date = new Date(params['dates']['start']);
-      this.end_date = new Date(params['dates']['end']);
+      	try {
+      		params = JSON.parse(localStorage['params'])
+      	}
+        catch(e){
+          console.debug(e);
+          params = {'weight':{'start': 0,
+                              'target': 0},
+                    'dates':{'start':0,
+                             'end': 0},
+                    }
+        
+         }
+        this.start_weight = params['weight']['start'];
+        this.target_weight = params['weight']['target'];
+        this.start_date = new Date(params['dates']['start']);
+        this.end_date = new Date(params['dates']['end']);
 
-      this.save = function(){
-      	localStorage['params'] = params;
+        this.save = function(){
+      	  params['weight']['start'] = this.start_weight;
+      	  params['weight']['target'] = this.target_weight;
+      	  params['dates']['start'] = this.start_date.toString();
+      	  params['dates']['end'] = this.end_date.toString();
+      	  localStorage['params'] = JSON.stringify(params);
+      };
+ 
+      this.cancel = function(){
+      	this.start_weight = params['weight']['start'];
+        this.target_weight = params['weight']['target'];
+        this.start_date = new Date(params['dates']['start']);
+        this.end_date = new Date(params['dates']['end']);
+      }
+
       }
     ]
   });
