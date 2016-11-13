e8fa9d671c4de7567aa14004374933eb54a9e4db
Eugene Feshchenko <efeshchenko@milosolutions.com>
2016-11-13 13:46:28 +0200

    some progres

--- app/weight-data/weight-data.component.js
+++ app/weight-data/weight-data.component.js
@@ -5,6 +5,25 @@ angular.
     controller: ['$http',
       function WeightDataController($http) {
         this.records = JSON.parse(localStorage['records']);
+
+        this.current_date = new Date();
+        this.current_date.setHours(0); 
+        this.current_date.setMinutes(0);
+        this.current_date.setSeconds(0);
+        this.current_date.setMilliseconds(0);
+
+
+        this.setWeight = function(){
+          var key = JSON.stringify(this.current_date);
+          key = JSON.parse(key);
+          for (var i=0; i<this.records.length; i++){
+            if (this.records[i]['date'] == key){
+              this.records[i]['actual'] = this.currentWeight;
+              localStorage['records'] = JSON.stringify(this.records);
+            }
+          }
+        }
+
         }
       ]
   });
