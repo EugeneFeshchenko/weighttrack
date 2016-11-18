b5a3becf2f87b05417a94bd15a5358dd503b930b
Eugene Feshchenko <efeshchenko@milosolutions.com>
2016-11-18 22:24:15 +0200

    checkpoint

--- app/record-form/record-form.component.js
+++ app/record-form/record-form.component.js
@@ -8,7 +8,29 @@ component('recordForm', {
             for (var i = 0; i < this.records.length; i++) {
                 this.records[i]['planned'] = parseFloat(this.records[i]['planned']);
                 this.records[i]['actual'] = parseFloat(this.records[i]['actual']);
+                this.records[i]['date'] = new Date(this.records[i]['date']);
             }
+
+            this.date = new Date();
+            this.date.setHours(0, 0, 0, 0);
+            for (var i = 0; i < this.records.length; i++) {                
+                if (this.records[i]['date'].getTime() == this.date.getTime()) {
+                    this.currentWeight = this.records[i]['actual'];
+                    this.plannedWeight = this.records[i]['planned'];
+                    this.note = this.records[i]['note'];
+                };
+            }
+
+            this.saveRecord = function() {
+                for (var i = 0; i < this.records.length; i++) {
+                    if (this.records[i]['date'].getTime() == this.date.getTime()) {
+                        this.records[i]['actual'] = this.currentWeight;
+                        this.records[i]['note'] = this.note;
+                        localStorage['records'] = JSON.stringify(this.records);
+                        window.location = '#';
+                    }
+                }
+            };
 
         } //controllerEnd;
     ]
--- app/weight-data/weight-data.component.js
+++ app/weight-data/weight-data.component.js
@@ -8,73 +8,12 @@ component('weightData', {
             for (var i = 0; i < this.records.length; i++) {
                 this.records[i]['planned'] = parseFloat(this.records[i]['planned']);
                 this.records[i]['actual'] = parseFloat(this.records[i]['actual']);
+                this.records[i]['date'] = new Date(this.records[i]['date']);
             }
 
-            this.current_date = new Date();
-            this.current_date.setHours(0, 0, 0, 0);
-            var key = JSON.stringify(this.current_date);
-            key = JSON.parse(key);
-            for (var i = 0; i < this.records.length; i++) {
-                if (this.records[i]['date'] == key) {
-                    this.currentWeight = this.records[i]['actual'];
-                };
-            }
+            this.today = new Date();
 
-            this.setWeight = function() {
-                for (var i = 0; i < this.records.length; i++) {
-                    if (this.records[i]['date'] == key) {
-                        this.records[i]['actual'] = this.currentWeight;
-                        localStorage['records'] = JSON.stringify(this.records);
-                    }
-                }
-                this.updateChart();
-            };
-
-            this.setRandomValues = function() {
-                for (var i = 0; i < this.records.length; i++) {
-                    var operand = Math.random() >= 0.5;
-                    var second = operand ? Math.random() : -1 * Math.random();
-                    var val = this.records[i]['planned'] + second;
-                    val = Math.round(val * 100) / 100;
-                    this.records[i]['actual'] = val;
-                }
-                this.updateChart();
-            }
-
-            // chart-related piece
-            this.updateChart = function() {
-                this.series = ['План', 'Факт'];
-                this.labels = [];
-                this.data = [
-                    [],
-                    []
-                ]
-                for (var i = 0; i < this.records.length; i++) {
-                    var date_string = new Date(this.records[i]['date']);
-                    date_string = date_string.getDate() + '/' + (date_string.getMonth() + 1);
-                    this.labels.push(date_string);
-                    this.data[0].push(this.records[i]['planned']);
-                    this.data[1].push(this.records[i]['actual']);
-                }
-
-                this.options = {
-                    scales: {
-                        yAxes: [{
-                            id: 'y-axis-1',
-                            type: 'linear',
-                            display: true,
-                            position: 'left'
-                        }, {
-                            id: 'y-axis-2',
-                            type: 'linear',
-                            display: true,
-                            position: 'right'
-                        }]
-                    }
-                }
-            }
-            this.updateChart();
-            //end chart-related-piece
+            
         } //controller end;
     ]
 });
